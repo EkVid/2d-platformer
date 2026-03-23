824b48a1f55d32b2d2575bd812f2ccde3bbe4ebc
Austin <austinjas014@gmail.com>
2026-03-23 15:31:44 -0400

    added enemy

--- scripts/KillZone.cs
+++ scripts/KillZone.cs
@@ -4,18 +4,25 @@ using System;
 public partial class KillZone : Area2D
 {
     private Timer _timer;
+
+    public override void _Ready()
+    {
+        _timer = GetNode<Timer>("Timer");
+    }
     
     public void OnBodyEntered(Node2D body)
     {
         if (body.Name == "player")
         {
-            _timer = GetNode<Timer>("Timer");
-            _timer.Start();   
+            _timer.WaitTime = 2f;
+            _timer.Start();
+            body.GetNode<CollisionShape2D>("CollisionShape2D").QueueFree();
         }
     }
 
     private void OnTimerTimeOut()
     {
+        _timer.WaitTime = 1;
         RestartGame();
     }
 
